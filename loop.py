84184ddab0a305d2820209d7b286fef19f955bb9
harishganesh1519 <60419032+harishganesh1519@users.noreply.github.com>
2020-01-29 14:13:40 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,28 @@
+a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+end=len(a)
+i=0
+for i in range (i,16) :
+     print(a[i])
+     
+     
+'''''sample output
+     a
+     b
+     c
+     d
+     e
+     f
+     g
+     h
+     i
+     j
+     k
+     l
+     m
+     n
+     o
+     p''''
+     
+   
+     
+  
